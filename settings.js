ebd3e4afab067580872eeb26a9981abfe5a232f9
d33l <dgdarrell8@gmail.com>
2025-08-23 18:11:58 +0200

    Update settings.js for Dran22

--- settings.js
+++ settings.js
@@ -1,8 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO-MD;;;2Z1WwZ4K#NEqC2w8INjaskJuqZG_CM35bnffT75S67-Hlu2_UHB8",
+  OWNER_NUMBER: "+256768847836", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
