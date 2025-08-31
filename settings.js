7f5f4803a0a33b9dd0e2f82b29cca368ab1a272a
d33l <dgdarrell8@gmail.com>
2025-08-31 20:49:04 +0200

    Update settings.js for Starbouwy

--- settings.js
+++ settings.js
@@ -1,8 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO-MD;;;CMk3mQJa#2EMi6TgmjO0fUAebjySLAu1ytf-_nIOkFnGWsLCdGUQ",
+  OWNER_NUMBER: "+254745693906", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
